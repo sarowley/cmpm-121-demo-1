4902ba9d56e5f66c7d115cadccef24b94f25d8b6
sarowley <sarowley@ucsc.edu>
2023-10-12 20:16:56 -0700

    step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ let multiplier: number = 0;
 let A_counter: number = 0;
 let B_counter: number = 0;
 let C_counter: number = 0;
+let A_check: number = 10;
+let B_check: number = 100;
+let C_check: number = 1000;
 
 const Count_A: HTMLDivElement = document.querySelector("#A_counter")!;
 Count_A.append(body);
@@ -41,41 +44,49 @@ button?.addEventListener("mousedown", function handleClick(event) {
 });
 
 const button1 = document.getElementById("btn1");
-
 button1?.addEventListener("mousedown", function handleClick(event) {
-  if (counter >= 10) {
-    counter -= 10;
+  if (counter >= A_check) {
+    counter -= A_check;
     header.innerHTML = "amount of otters: " + counter;
     multiplier += 0.1;
     A_counter += 1;
+    A_check *= 1.15;
     body.innerHTML = "rate: " + multiplier.toFixed(1);
     Count_A.innerHTML = "num of A: " + A_counter;
+    document.querySelector("#btn1")!.innerHTML =
+      "(A upgrade (cost: " + A_check.toFixed(1) + ")";
   }
   console.log(event);
 });
 
 const button2 = document.getElementById("btn2");
 button2?.addEventListener("mousedown", function handleClick(event) {
-  if (counter >= 100) {
-    counter -= 100;
+  if (counter >= B_check) {
+    counter -= B_check;
     header.innerHTML = "amount of otters: " + counter;
     multiplier += 2;
     B_counter += 1;
+    B_check *= 1.15;
     body.innerHTML = "rate: " + multiplier.toFixed(1);
     Count_B.innerHTML = "num of B: " + B_counter;
+    document.querySelector("#btn2")!.innerHTML =
+      "(B upgrade (cost: " + B_check.toFixed(1) + ")";
   }
   console.log(event);
 });
 
 const button3 = document.getElementById("btn3");
 button3?.addEventListener("mousedown", function handleClick(event) {
-  if (counter >= 1000) {
-    counter -= 1000;
+  if (counter >= C_check) {
+    counter -= C_check;
     header.innerHTML = "amount of otters: " + counter;
     multiplier += 50;
     C_counter += 1;
+    C_check *= 1.15;
     body.innerHTML = "rate: " + multiplier.toFixed(1);
     Count_C.innerHTML = "num of C: " + C_counter;
+    document.querySelector("#btn3")!.innerHTML =
+      "(C upgrade (cost: " + C_check.toFixed(1) + ")";
   }
   console.log(event);
 });
